4445fb3ddbadb8e19f390e802f1d3829612dc5e9
Lars Trieloff <lars@trieloff.net>
2017-02-28 17:24:22 +0100

    Switch from token to code response type

--- authenticate.sh
+++ authenticate.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Replace this with your own Client ID
 CLIENT_ID="8d06fe64c9ea43b4a41adf9348dec9ae"
-open "https://ims-na1-stg1.adobelogin.com/ims/authorize/v1?client_id=$CLIENT_ID&response_type=token&redirect_uri=https://requestb.in/y4nwg5y4&scope=openid,creative_sdk"
+open "https://ims-na1-stg1.adobelogin.com/ims/authorize/v1?response_type=code&client_id=$CLIENT_ID&scope=AdobeID%2Copenid%2Ccreative_sdk&redirect_uri=https://requestb.in/y4nwg5y4"
+#open "https://ims-na1-stg1.adobelogin.com/ims/authorize/v1?client_id=$CLIENT_ID&response_type=token&redirect_uri=https://requestb.in/y4nwg5y4&scope=openid,creative_sdk"
 sleep 10
 REDIRECT=`osascript -e 'tell Application "Safari" to return URL of front document'`
-TOKEN=`echo $REDIRECT | sed -e "s/.*#access_token=//" | sed -e "s/&.*//"`
+TOKEN=`echo $REDIRECT | sed -e "s/.*code=//" | sed -e "s/&.*//"`
+#TOKEN=`echo $REDIRECT | sed -e "s/.*#access_token=//" | sed -e "s/&.*//"`
 echo $TOKEN | pbcopy
 #TOKEN=`pbpaste`
 echo ""
