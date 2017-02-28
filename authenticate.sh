84338dda8fb8022e5e2538995864d845bb8a42d0
Lars Trieloff <lars@trieloff.net>
2017-02-28 13:47:57 +0100

    Make curl request

--- authenticate.sh
+++ authenticate.sh
@@ -2,8 +2,40 @@
 # Replace this with your own Client ID
 CLIENT_ID="8d06fe64c9ea43b4a41adf9348dec9ae"
 open "https://ims-na1-stg1.adobelogin.com/ims/authorize/v1?client_id=8d06fe64c9ea43b4a41adf9348dec9ae&response_type=token&redirect_uri=https://requestb.in/y4nwg5y4&scope=openid,creative_sdk"
-sleep 3
+sleep 10
 REDIRECT=`osascript -e 'tell Application "Safari" to return URL of front document'`
 TOKEN=`echo $REDIRECT | sed -e "s/.*#access_token=//" | sed -e "s/&.*//"`
+echo $TOKEN | pbcopy
+#TOKEN=`pbpaste`
+echo ""
 echo "Got this token:"
 echo $TOKEN
+
+echo "Running: "
+echo curl -vv --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $TOKEN" --header "x-api-key: $CLIENT_ID" -d '{ \ 
+   "client_id": '"$1"', \ 
+   "name": "Image uploaded", \ 
+   "description": "Image uploaded, duh", \ 
+   "webhook_url": "http://requestb.in/1mmkhdb1", \ 
+   "events_of_interest": [ \ 
+     { \ 
+       "event_code": "sc_asset_created", \ 
+       "provider": "sc" \ 
+     } \ 
+   ] \ 
+}' 'https://csm-dev.adobe.io/csm/users/webhooks'
+
+Sleep 1
+
+curl -vv --header 'Content-Type: application/json' --header 'Accept: application/json' --header "Authorization: Bearer $TOKEN" --header "x-api-key: $CLIENT_ID" -d '{ \ 
+   "client_id": '"$1"', \ 
+   "name": "Image uploaded", \ 
+   "description": "Image uploaded, duh", \ 
+   "webhook_url": "http://requestb.in/1mmkhdb1", \ 
+   "events_of_interest": [ \ 
+     { \ 
+       "event_code": "sc_asset_created", \ 
+       "provider": "sc" \ 
+     } \ 
+   ] \ 
+}' 'https://csm-dev.adobe.io/csm/users/webhooks'
