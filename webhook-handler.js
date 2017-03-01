1d10632b0df25caa417fb30996f444a9e2d7ebab
Lars Trieloff <lars@trieloff.net>
2017-03-01 14:29:50 +0100

    Basic handler code

---
+++ webhook-handler.js
@@ -0,0 +1,8 @@
+/**
+ * WebHook handler for Adobe I/O Events.
+ * @param challenge challenge required for registering WebHook
+ */
+function main(params) {
+    var challenge = params.challenge;
+    return challenge;
+}
