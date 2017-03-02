e00d5d3ee8ab63ebff2e322286614ccea92cb521
Lars Trieloff <lars@trieloff.net>
2017-03-02 20:23:03 +0100

    Removed dead code

--- webhook-handler.js
+++ webhook-handler.js
@@ -94,19 +94,4 @@ function main(params) {
       });
     });
   }
-  if (params.asset && params.asset.mime_type) {
-    if (params.asset.mime_type=="image/jpeg") {
-      return changeColor("red").then(function(body) {
-        return {"color": "red", "response": body};
-      });
-    } else if (params.asset.mime_type=="image/png") {
-      return changeColor("green").then(function(body) {
-        return {"color": "green", "response": body};
-    });
-    } else {
-      return {"color":"blue"};
-  }
-  } else {
-    return {"echo": params};	
-  }
 }
