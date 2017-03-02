3aae717d579eb6efe402e369b01e672dc9665cac
Lars Trieloff <lars@trieloff.net>
2017-03-02 15:34:35 +0100

    Get colors from Creative Cloud metadata

--- webhook-handler.js
+++ webhook-handler.js
@@ -1,16 +1,22 @@
 var request = require('request-promise');
 
+var api_key = "8d06fe64c9ea43b4a41adf9348dec9ae"
 
 function changeColor(color) {
-	var options = {
-		"host":"maker.ifttt.com",
-		//"host":"example.com",
-		"path":"/trigger/" + color + "/with/key/bLghUjKHwCSv9rqZeXdSxq",
-		"method":"GET"
-	};
-	
-	return request({"method":"POST", "uri": "http://maker.ifttt.com/trigger/" + color + "/with/key/bLghUjKHwCSv9rqZeXdSxq"})
+	return request({"method":"POST", "uri": "http://maker.ifttt.com/trigger/" + color + "/with/key/bLghUjKHwCSv9rqZeXdSxq"});
 }
+
+function getColors(assetUrn, token) {
+	return request({
+		"method":"GET", 
+		"uri": "https://cc-api-storage-stage.adobe.io/id/" + assetUrn + "/:metadata", 
+		"headers": {"x-api-key": api_key, "Authorization": "Bearer " + token, "Accept": "application/vnd.adobe.file+json"}, 
+		"json": true}).then(function(body) {
+			console.log(body.kuler.rgb);
+		});
+}
+
+
 /*
 changeColor("red").then(function(body) {
 	console.log("Request has been made");
