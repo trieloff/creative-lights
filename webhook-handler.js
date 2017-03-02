46a27f3bd9e1a0e8f3500a0bffb6dd18db0f6a29
Lars Trieloff <lars@trieloff.net>
2017-03-02 12:47:39 +0100

    Change the lights in Lars’ home office

--- webhook-handler.js
+++ webhook-handler.js
@@ -1,4 +1,18 @@
-var http = require("http");
+var https = require("https");
+
+
+function changeColor(color) {
+	var options = {
+		"host":"maker.ifttt.com",
+		//"host":"example.com",
+		"path":"/trigger/" + color + "/with/key/bLghUjKHwCSv9rqZeXdSxq",
+		"method":"GET"
+	};
+	https.request(options, function(res){
+		console.log(res);
+		console.log("Request has been made");
+	}).end();
+}
 
 /**
  * WebHook handler for Adobe I/O Events.
@@ -11,8 +25,10 @@ function main(params) {
     }
 	if (params.asset && params.asset.mime_type) {
 		if (params.asset.mime_type=="image/jpeg") {
+			changeColor("red");
 			return {"color":"red"};
 		} else if (params.asset.mime_type=="image/png") {
+			changeColor("green");
 			return {"color":"green"};
 		} else {
 			return {"color":"blue"};
