eb90e7fa33ffc772a8fff90a8884b5b63fe96cdd
Lars Trieloff <lars@trieloff.net>
2017-03-03 23:35:36 +0100

    Cut dead code

--- webhook-handler.js
+++ webhook-handler.js
@@ -106,26 +106,14 @@ function main(params) {
     return { "challenge": challenge };
   }
   if (params.asset && params.asset.urn) {
-    //return {"secret": params.secret,
-    //        "bridge": params.bridge,
-    //        "token" : decrypt(secrettoken, params.secret)};
-    
-    //setLight(params.bridge, 45, {"on":true, "sat":254, "bri":254,"hue":Math.round(Math.random()*65000)});
-    //setLight(params.bridge, 48, {"on":true, "sat":254, "bri":254,"hue":Math.round(Math.random()*65000)});
-    
     return getColors(params.asset.urn, decrypt(secrettoken, params.secret)).then(function(colors) {
-      //TODO: actually iterate through the colors and change the hue lamps.
-      //return colors;
-      //var lights = [45, 48];
-      
       return getLights(params.bridge).then(function(lights) {
         var responses = lights.map(function(light, i) {
-          return setLight(params.bridge, lights[i], colors.colors[i%5]);
+          return setLight(params.bridge, light, colors.colors[i%5]);
         });
         
         return Promise.all(responses);
       });
     });
-    
   }
 }
