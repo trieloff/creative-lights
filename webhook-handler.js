d693b3d5b0f558fa81168cb01743b23c416cd2fd
Lars Trieloff <lars@trieloff.net>
2017-03-03 22:53:19 +0100

    Get the lights

--- webhook-handler.js
+++ webhook-handler.js
@@ -110,11 +110,16 @@ function main(params) {
     //        "bridge": params.bridge,
     //        "token" : decrypt(secrettoken, params.secret)};
     
-    getLights(params.bridge).then(function(lights) {
+    //setLight(params.bridge, light, {"on":true, "sat":254, "bri":254,"hue":Math.round(Math.random()*65000)});
+    
+    return getLights(params.bridge).then(function(lights) {
+      return {"lights": lights};
+      /*
       lights.map(function(light, i) {
         console.log(i % 5);
         setLight(params.bridge, light, {"on":true, "sat":254, "bri":254,"hue":Math.round(Math.random()*65000)});
       });
+      */
     });
     
     return getColors(params.asset.urn, decrypt(secrettoken, params.secret)).then(function(colors) {
