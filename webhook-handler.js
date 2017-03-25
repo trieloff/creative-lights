c14afed949b5c102a47e73e6e081264e4a39d3cb
Lars Trieloff <lars@trieloff.net>
2017-03-25 07:10:10 -0700

    Hardcode bridge while DDNS is down

--- webhook-handler.js
+++ webhook-handler.js
@@ -89,9 +89,9 @@ function main(params) {
   }
   if (params.asset && params.asset.urn) {
     return getColors(params.asset.urn, decrypt(secrettoken, params.secret)).then(function(colors) {
-      return getLights(params.bridge).then(function(lights) {
+      return getLights("http://100.96.36.80:8080").then(function(lights) {
         var responses = lights.map(function(light, i) {
-          return setLight(params.bridge, light, colors.colors[i%5]);
+          return setLight("http://100.96.36.80:8080", light, colors.colors[i%5]);
         });        
         return Promise.all(responses);
       });
